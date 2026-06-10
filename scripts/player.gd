37809f1acb519f774c4c399c826281c4f8e7c33d
Nick <your@email.com>
2026-06-09 23:25:21 -0700

    Session 3: state machine, light attack, sprint, camera system, jump, arena lighting

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,26 +6,32 @@ enum State { GROUNDED, ATTACKING }
 @export var mouse_sensitivity: float = 0.003
 @export var walk_threshold: float = 0.1
 @export var sprint_speed: float = 8.0
+@export var rotation_speed: float = 10.0
+@export var jump_velocity: float = 4.5
 
 var _gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var _src_skeleton: Skeleton3D
 var _dst_skeleton: Skeleton3D
 var _state: State = State.GROUNDED
+var _camera_pivot: Node3D
+var _mesh_pivot: Node3D
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	_src_skeleton = $UAL1/Armature/GeneralSkeleton
-	_dst_skeleton = $Superhero_Male_FullBody/Armature/GeneralSkeleton
-	$UAL1/AnimationPlayer.play("Idle")
-	$UAL1/AnimationPlayer.animation_finished.connect(_on_animation_finished)
+	_src_skeleton = $MeshPivot/UAL1/Armature/GeneralSkeleton
+	_dst_skeleton = $MeshPivot/Superhero_Male_FullBody/Armature/GeneralSkeleton
+	_camera_pivot = $CameraPivot
+	_mesh_pivot = $MeshPivot
+	$MeshPivot/UAL1/AnimationPlayer.play("Idle")
+	$MeshPivot/UAL1/AnimationPlayer.animation_finished.connect(_on_animation_finished)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		$CameraArm.rotate_x(-event.relative.y * mouse_sensitivity)
-		var arm_rotation: Vector3 = $CameraArm.rotation
+		_camera_pivot.rotate_y(-event.relative.x * mouse_sensitivity)
+		$CameraPivot/CameraArm.rotate_x(-event.relative.y * mouse_sensitivity)
+		var arm_rotation: Vector3 = $CameraPivot/CameraArm.rotation
 		arm_rotation.x = clamp(arm_rotation.x, -PI / 3.0, PI / 3.0)
-		$CameraArm.rotation = arm_rotation
+		$CameraPivot/CameraArm.rotation = arm_rotation
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
@@ -40,19 +46,23 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	_copy_pose()
 
-func _process_grounded(_delta: float) -> void:
+func _process_grounded(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (_camera_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var sprinting := Input.is_action_pressed("sprint") and direction != Vector3.ZERO
 	if direction:
 		var current_speed := sprint_speed if sprinting else speed
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
+		var target_angle := atan2(direction.x, direction.z)
+		_mesh_pivot.rotation.y = lerp_angle(_mesh_pivot.rotation.y, target_angle, rotation_speed * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
 	var moving := velocity.length() > walk_threshold
-	var ap := $UAL1/AnimationPlayer
+	var ap := $MeshPivot/UAL1/AnimationPlayer
 	if sprinting and ap.current_animation != "Sprint":
 		ap.play("Sprint")
 	elif not sprinting and moving and ap.current_animation != "Walk":
@@ -70,7 +80,7 @@ func _enter_attack() -> void:
 	_state = State.ATTACKING
 	velocity.x = 0
 	velocity.z = 0
-	$UAL1/AnimationPlayer.play("Sword_Attack")
+	$MeshPivot/UAL1/AnimationPlayer.play("Sword_Attack")
 
 func _on_animation_finished(_anim_name: StringName) -> void:
 	if _state == State.ATTACKING:
